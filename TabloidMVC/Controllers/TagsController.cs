3da3e95b17053033ada2f0305bd411601dc67e51
Morganero <morganero1@gmail.com>
2023-01-10 15:57:09 -0600

    Did All Tags and Edit Tags Tickets

---
+++ TabloidMVC/Controllers/TagsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class TagsController : Controller
+    {
+        private readonly ITagRepository _tagRepo;
+        public TagsController(ITagRepository tagRepository)
+        {
+            _tagRepo = tagRepository;
+        }
+        // GET: TagController
+        public ActionResult Index()
+        {
+            //public List<Tag> GetAllTags
+            List<Tag> tags = _tagRepo.GetAllTags();
+            return View(tags);
+        }
+
+        // GET: TagController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TagController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TagController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TagController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Tag tag = _tagRepo.GetTagById(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+            return View(tag);
+        }
+
+        // POST: TagController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Tag tag)
+        {
+            try
+            {
+                _tagRepo.UpdateTag(tag);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View(tag);
+            }
+        }
+
+        // GET: TagController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TagController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
