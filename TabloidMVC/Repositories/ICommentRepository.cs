05936322eca4d7e468502a86783ea62db980ade1
Ryan Allred <ryanallred91@gmail.com>
2023-01-11 11:00:57 -0600

    my comment stuff

---
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+         List<Comment> GetAllPostedComments();
+         List<Comment> GetCommentsByPostId(int postId);
+         void AddComment(Comment comment);
+    }
+}
