5980d2c287ad64a5de41247a6cfa0fb989e75efa
Morganero <morganero1@gmail.com>
2023-01-12 14:32:17 -0600

    Added List of Profiles

--- TabloidMVC/Repositories/UserProfileRepository.cs
+++ TabloidMVC/Repositories/UserProfileRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 using TabloidMVC.Models;
 using TabloidMVC.Utils;
 
@@ -50,6 +52,48 @@ namespace TabloidMVC.Repositories
                     reader.Close();
 
                     return userProfile;
+                }
+            }
+        }
+        public List<UserProfile> GetAllUserProfiles()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT up.Id, up.FirstName, up.LastName, up.ImageLocation, up.DisplayName, up.Email, up.CreateDateTime, up.UserTypeId, ut.Name 
+                        FROM UserProfile up Join UserType ut
+                        ON up.UserTypeId = ut.Id
+                    ";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<UserProfile> userProfiles = new List<UserProfile>();
+                        while (reader.Read())
+                        {
+                            UserProfile userProfile = new UserProfile
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                //ImageLocation = !reader.IsDBNull(reader.GetOrdinal("ImageLocation")) ? reader.GetString(reader.GetOrdinal("ImageLocation")) : null,
+                                DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                                Email = reader.GetString(reader.GetOrdinal("Email")),
+                                //CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                                
+                            };
+                            UserType userType = new UserType
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+                            userProfile.UserType = userType;
+                            userProfiles.Add(userProfile);
+                        }
+
+                        return userProfiles;
+                    }
                 }
             }
         }
